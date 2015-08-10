7fd45360ee31fc5191bd664181be024e154d4608
Ben van Vliet <benvliet@me.com>
2015-08-10 17:27:54 +0200

    Update 20150101_DSUR_h08.R

--- 20150101_DSUR_h08.R
+++ 20150101_DSUR_h08.R
@@ -91,13 +91,13 @@ anova(eelModel.1, eelModel.2) #alternative, compare model1 and model 2
 
 
 #casewise diagnostics
-#check if cases exert undue influence on model
+#check if cases exert undue influence on model; check 7.7 for more info
 eelData$predicted.probabilities <- fitted(eelModel.1)
-eelData$standardized.residuals <- rstandard(eelModel.1) #only 5% should lie outside 1.96, above 3 is cause for concern; check 7.7 for more info
+eelData$standardized.residuals <- rstandard(eelModel.1) #only 5% should lie outside 1.96, above 3 is cause for concern
 eelData$studentized.residuals <- rstudent(eelModel.1) #only 5% should lie outside 1.96, above 3 is cause for concern
 eelData$dfbeta <- dfbeta(eelModel.1) #should be less than 1
 eelData$dffit <- dffits(eelModel.1) #should be less than 1
-eelData$leverage <- hatvalues(eelModel.1) #lies between 0-1, expected value: number of predictors + 1 / number of cases (e.g. 2/113 = 0.018)
+eelData$leverage <- hatvalues(eelModel.1) #lies between 0-1, expected value: number of predictors + 1 / n (e.g. 2/113 = 0.018)
 
 head(eelData)
 
