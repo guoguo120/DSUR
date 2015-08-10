a5038ee5c303e6e4b4da628ec83ee866fb946108
Ben van Vliet <benvliet@me.com>
2015-08-10 17:26:21 +0200

    Update 20150101_DSUR_h08.R

--- 20150101_DSUR_h08.R
+++ 20150101_DSUR_h08.R
@@ -1,6 +1,6 @@
 #H8 Logistic regression
 
-#When assumption of linear relationship is violated = logarithmic transformation, equation expresses the prob of Y occuring
+#When assumption of linear relationship is violated -> logarithmic transformation; equation is prob of Y occuring
 
 
 #Principles of log regression
