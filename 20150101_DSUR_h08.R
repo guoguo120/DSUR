acece9324b8b9a71d63efbba3401b90742ca1827
Ben van Vliet <benvliet@me.com>
2015-08-14 14:48:33 +0200

    Update 20150101_DSUR_h08.R

--- 20150101_DSUR_h08.R
+++ 20150101_DSUR_h08.R
@@ -68,11 +68,11 @@ logisticPseudoR2s <- function(LogModel) {
     modelN <- length(LogModel$fitted.values)
     R.hl <- 1 - dev / nullDev
     R.cs <- 1 - exp ( -(nullDev - dev) / modelN)
-    R.n <- R.cs / ( 1 - ( exp (-(nullDev / modelN))))
+    R.nk <- R.cs / ( 1 - ( exp (-(nullDev / modelN))))
     cat("Pseudo R^2 for logistic regression\n")
     cat("Hosmer and Lemeshow R^2  ", round(R.hl, 3), "\n")
     cat("Cox and Snell R^2        ", round(R.cs, 3), "\n")
-    cat("Nagelkerke R^2           ", round(R.n, 3),  "\n")
+    cat("Nagelkerke R^2           ", round(R.nk, 3), "\n")
     }
 
 logisticPseudoR2s(eelModel.0)
